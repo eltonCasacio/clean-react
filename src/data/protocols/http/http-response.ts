9d501b8286a3dd092ebc1fd4b7d35043c09d83e5
eltoncasacio <eltoncasacio@hotmail.com>
2022-09-11 19:32:50 -0300

    feat: ensure remoteAuthentication throw invalidCredentials

---
+++ src/data/protocols/http/http-response.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export enum HttpStatusCode {
+    noContent = 204,
+    unauthorized = 401,
+}
+export type HttpResponse = {
+    statusCode: HttpStatusCode
+    body?: any
+}
