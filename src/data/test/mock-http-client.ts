eb73cf8040073da4d810015e2a7cd1a5c8b36749
eltoncasacio <eltoncasacio@hotmail.com>
2022-09-11 19:33:12 -0300

    test: ensure remoteAuthentication throw invalidCredentials

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,13 @@
 import { HttpPostClient, HttpPostClientParams } from "@/data/protocols/http/http-post-client"
+import { HttpResponse, HttpStatusCode } from "@/data/protocols/http/http-response"
 
 export class HttpPostClientSpy implements HttpPostClient {
     url?: string
     body?: object
-    async post(params: HttpPostClientParams): Promise<void> {
+    response: HttpResponse = {statusCode :  HttpStatusCode.noContent}
+    async post(params: HttpPostClientParams): Promise<HttpResponse> {
         this.url = params.url
         this.body = params.body
-        return Promise.resolve()
+        return Promise.resolve(this.response)
     }
 }
--- src/data/usecases/authentication/remote-authentication.test.ts
+++ src/data/usecases/authentication/remote-authentication.test.ts
@@ -2,6 +2,8 @@ import { HttpPostClientSpy } from "@/data/test/mock-http-client"
 import { RemoteAuthentication } from "./remote-authentication"
 import { faker } from '@faker-js/faker'
 import { mockAuthentication } from "@/domain/test/mock-authentication"
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials.error"
+import { HttpStatusCode } from "@/data/protocols/http/http-response"
 
 type SutTypes = {
     sut: RemoteAuthentication
@@ -31,5 +33,14 @@ describe("RemoteAuthentication", () => {
         const body = mockAuthentication()
         await sut.authenticate(body)
         expect(httpPostClientSpy.body).toEqual(body)
+    })
+
+    test("should throw InvalidCredentialsError if httpPostClient returns 401", async () => {
+        const {sut, httpPostClientSpy} = sutFactory()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unauthorized
+        }
+        const promise = sut.authenticate(mockAuthentication())
+        await expect(promise).rejects.toThrow(new InvalidCredentialsError())
     })
 })
