a1b26020e32b21b686d0bc0c9cad54413e645901
eltoncasacio <eltoncasacio@hotmail.com>
2022-09-11 15:59:48 -0300

    test: ensure remote authentication calls httpPostClient with correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { HttpPostClientSpy } from "../../test/mock-http-client"
 import { RemoteAuthentication } from "./remote-authentication"
 import { faker } from '@faker-js/faker'
+import { mockAuthentication } from "../../../domain/test/mock-authentication"
 
 type SutTypes = {
     sut: RemoteAuthentication
@@ -21,7 +22,14 @@ describe("RemoteAuthentication", () => {
     test("should call httpPostClient with correct url", async () => {
         const url = faker.internet.url()
         const {sut, httpPostClientSpy} = sutFactory(url)
-        await sut.authenticate()
+        await sut.authenticate(mockAuthentication())
         expect(httpPostClientSpy.url).toBe(url)
+    })
+
+    test("should call httpPostClient with correct body", async () => {
+        const {sut, httpPostClientSpy} = sutFactory()
+        const body = mockAuthentication()
+        await sut.authenticate(body)
+        expect(httpPostClientSpy.body).toEqual(body)
     })
 })
---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,9 @@
+import { AuthenticationParams } from "../usecases/authentication";
+import {faker} from '@faker-js/faker'
+
+export const mockAuthentication = (): AuthenticationParams => {
+    return {
+        username: faker.internet.email(),
+        password: faker.internet.password()
+    }
+}
