989fcb578104869b8d61f179fe7394b050004654
eltoncasacio <eltoncasacio@hotmail.com>
2022-09-12 13:45:58 -0300

    feat: ensure axios-http-client return correct values

--- src/infra/adapters/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/adapters/http/axios-http-client/axios-http-client.spec.ts
@@ -5,6 +5,12 @@ import { HttpPostClientParams } from "@/data/protocols/http"
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxiosResult = {
+    data: faker.helpers.objectValue<any>({}),
+    status: faker.random.alphaNumeric()
+}
+
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
 
 const sutFactory = (): AxiosHttpClient => {
     return new AxiosHttpClient()
@@ -21,5 +27,14 @@ describe("AxiosHttpClient", () => {
         const sut = sutFactory()
         await sut.post({url: request.url})
         expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+    })
+
+    test("should return the correct statusCode and body", async() => {
+        const sut = sutFactory()
+        const httpResponse = await sut.post(mockPostRequest())
+        expect(httpResponse).toEqual({
+            statusCode: mockedAxiosResult.status,
+            body: mockedAxiosResult.data
+        })
     })
 })
--- src/infra/adapters/http/axios-http-client/axios-http-client.ts
+++ src/infra/adapters/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,12 @@
-import { HttpPostClientParams } from "@/data/protocols/http";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpPostClient, HttpPostClientParams, HttpResponse } from "@/data/protocols/http";
 import axios from 'axios'
-
-export class AxiosHttpClient {
-    async post (params: HttpPostClientParams<any>): Promise<void> {
-        await axios.post(params.url, params.body)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+    async post(params: HttpPostClientParams<any>): Promise<HttpResponse<any>> {
+        const { data, status } = await axios.post(params.url, params.body)
+        return {
+            statusCode: status,
+            body: data
+        }
     }
 }
