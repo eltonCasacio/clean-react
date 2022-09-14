56395e0df1839330fe7025a6fbf91764972e6dae
eltoncasacio <eltoncasacio@hotmail.com>
2022-09-13 22:10:29 -0300

    chore: config webpack

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-export const index = () => {
-  return (
-    <div>index</div>
-  )
-}
+const root = ReactDOM.createRoot(document.getElementById("main") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <h1>Clean Architecture React</h1>
+  </React.StrictMode>
+);
