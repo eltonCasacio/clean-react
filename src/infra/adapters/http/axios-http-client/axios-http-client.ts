91905448f236796c06dcc69f843f9a616b94952a
eltoncasacio <eltoncasacio@hotmail.com>
2022-09-13 21:24:38 -0300

    chore: config webpack

--- src/infra/adapters/http/axios-http-client/axios-http-client.ts
+++ src/infra/adapters/http/axios-http-client/axios-http-client.ts
@@ -2,11 +2,11 @@
 import { HttpPostClient, HttpPostClientParams, HttpResponse } from "@/data/protocols/http";
 import axios from 'axios'
 export class AxiosHttpClient implements HttpPostClient<any, any> {
-    async post(params: HttpPostClientParams<any>): Promise<HttpResponse<any>> {
-        const { data, status } = await axios.post(params.url, params.body)
-        return {
-            statusCode: status,
-            body: data
-        }
+  async post(params: HttpPostClientParams<any>): Promise<HttpResponse<any>> {
+    const { data, status } = await axios.post(params.url, params.body)
+    return {
+      statusCode: status,
+      body: data
     }
+  }
 }
