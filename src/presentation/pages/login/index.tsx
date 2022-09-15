d092ab1dc9060c8c2422874c9f9201959a650bc8
eltoncasacio <eltoncasacio@hotmail.com>
2022-09-15 13:45:00 -0300

    refactor: add index file in components folder

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import Styles from "./login-styles.scss";
-import LoginHeader from "@/presentation/components/login-header/login-header";
-import Footer from "@/presentation/components/footer/footer";
-import Input from "@/presentation/components/input/input";
-import FormStatus from "@/presentation/components/form-status/form-status";
+import { LoginHeader, Footer, Input, FormStatus } from "@/presentation/components";
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-     <LoginHeader />
+      <LoginHeader />
       <form action="" className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" className="email" placeholder="Digite seu e-mail"/>
-        <Input  type="password" className="password" placeholder="Digite sua senha"/>
-       
-        <button className={Styles.submit} type="submit">Entrar</button>
+        <Input type="email" className="email" placeholder="Digite seu e-mail" />
+        <Input type="password" className="password" placeholder="Digite sua senha" />
+
+        <button className={Styles.submit} type="submit">
+          Entrar
+        </button>
         <span className={Styles.signup}>Criar Conta</span>
-       <FormStatus />
+        <FormStatus />
       </form>
       <Footer />
     </div>
